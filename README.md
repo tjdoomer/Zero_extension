58c93cb9822851a29d1e60ab3fd7ad7bbb987585
tjdoomer <tj@surfthechaos.net>
2025-01-30 07:54:20 +1100

    Zero Extension
    Necessary files required to run the VS code extension

--- README.md
+++ README.md
@@ -1,3 +1,77 @@
-# Zero_extension
-LLM VS code extension
-Creating a VS code extension following fireship.io tutorial in order to run Local LLM's.
+# zero-ext README
+
+This is the README for a VS code extension "zero-ext" used to run Local LLM's..
+Built following fireship.io tutorial.
+
+## Features
+
+This VS extension works in conjuction with Ollama and run models such as DeepSeek.
+
+
+## Requirements
+
+The model being run will be dependendant on your computer hardware, I recommend starting with a lower model e.g. DeepSeek-R1-Distill-Qwen-1.5B if you are unsure.
+
+Rough guide:
+ - 8gb ram - DeepSeek-R1-Distill-Qwen-1.5B
+ - 16gb ram - DeepSeek-R1-Distill-Llama-8B
+ - 32gb ram - DeepSeek-R1-Distill-Qwen-14B
+
+
+## Known Issues
+
+Initial tests with DeepSeek have resulted in some halucinations if instructions are not clear.
+
+## Release Notes
+
+Users appreciate release notes as you update your extension.
+
+### 1.0.0
+
+Initial release of ...
+
+//### 1.0.1
+
+
+
+## Guide & Requirements
+
+This project requires the following dependencies,
+
+https://nodejs.org/en 
+
+https://yeoman.io
+
+
+
+Implement this VS code extensionm template, 
+
+npx --package yo --package generator-code -- yo code
+
+alternate extension build https://www.npmjs.com/package/generator-code
+
+
+
+Download and install Ollama from https://ollama.com or https://github.com/ollama/ollama/blob/main/docs/faq.md#how-does-ollama-handle-concurrent-requests
+
+ollama run llama3.2
+
+
+Within ollama download the model you would like to use, 
+e.g. Deepseek https://ollama.com/library/deepseek-r1:8b
+- ensure the model is appropriate for the spec of the machine it is being run on.
+
+
+Once installed replace the extension.ts, package.json and the tsconfig.json in your VS code extension path.
+
+To get it up and running you can run the >debug mode in VS code
+Followed by cmd + shift + p
+Run the extension and you should now be running a local VS code LLM.
+
+
+Check out Fireship.io for more tutorials,
+https://www.youtube.com/watch?v=clJCDHml2cA
+https://fireship.io/courses/
+
+
+**Enjoy!**
---
+++ src/extension.ts
@@ -0,0 +1,94 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+import ollama from 'ollama';
+
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratz, "zero-ext" is now alive!');
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with registerCommand
+	// The commandId parameter must match the command field in package.json
+	const disposable = vscode.commands.registerCommand('zero-ext.hi', () => {
+		// The code you place here will be executed every time your command is executed
+		// Display a message box to the user
+		const panel = vscode.window.createWebviewPanel(
+			'deepChat',
+			'Deep Seek Chart',
+			vscode.ViewColumn.One,
+			{ enableScripts: true }
+		);
+		panel.webview.html = getWebviewContent();
+
+		panel.webview.onDidReceiveMessage(async (message: any) => {
+			if (message.command === 'chat') {
+				const userPrompt = message.text;
+				let responseText = '';
+
+				try {
+					const streamResponse = await ollama.chat({
+						model: 'deepseek-r1:8b',
+						messages: [{ role: 'user', content: userPrompt }],
+						stream: true
+					});
+
+					for await (const part of streamResponse) {
+						responseText += part.message.content;
+						panel.webview.postMessage({ command: 'chatResponse', text: responseText });
+					}
+
+					// response = await deepSeek(userPrompt);
+				}catch (err) {
+					panel.webview.postMessage({ command: 'chatResponse', text: 'Error: ' + String(err) });
+				}
+			}
+		});
+		//vscode.window.showInformationMessage('Hey zero-ext!');
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+function getWebviewContent(): string {
+	return /*html*/`
+	<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<style>
+		body { font-family: sans-serif; margin: 1 rem; }
+		#prompt { width: 100%; box-sizing: border-box; }
+		#response { nprder: 1px solid #ccc; margin-top: 1rem; padding: 0.5rem; min-height: 100px; }
+		</style>
+	</head>
+	<body>
+		<h1>Zero Extension</h1>
+		<textarea id="prompt" rows="3" placeholder="Enter your demands..."></textarea><br />
+		<button id="askBtn">Send it</button>
+		<div id="response"></div>
+
+		<script>
+			const vscode = acquireVsCodeApi();
+
+			document.getElementById('askBtn').addEventListener('click', () => {
+				const text = document.getElementById('prompt').value;
+				vscode.postMessage({ command: 'chat', text });
+			});
+
+			window.addEventListener('message', event => {
+				const { command, text } = event.data;
+				if (command === 'chatResponse') {
+					document.getElementById('response').innerText = text;
+				}
+			});
+		</script>
+	</body>
+	</html>`;
+}
+// This method is called when your extension is deactivated
+export function deactivate() {}
